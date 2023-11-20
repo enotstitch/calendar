e55861c0102ede23d496fc4f3ea4c9708f96ea49
enotstitch <kessler.anna96@gmail.com>
2023-11-20 09:38:48 +0300

    Реализация календаря

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function createCalendar(elem, year, month) {
+  elem = document.querySelector(elem);
+
+  let mon = month - 1;
+  let d = new Date(year, mon);
+
+  let table = `
+    <table>
+    <tbody>
+      <tr>
+        <th>Пн</th>
+        <th>Вт</th>
+        <th>Ср</th>
+        <th>Чт</th>
+        <th>Пт</th>
+        <th>Сб</th>
+        <th>Вс</th>
+      </tr>
+      <tr>
+  `;
+
+  for (let i = 0; i < getDay(d); i++) {
+    table += '<td></td>';
+  }
+
+  while (d.getMonth() == mon) {
+    table += '<td>' + d.getDate() + '</td>';
+    if (getDay(d) % 7 == 6) {
+      table += '</tr><tr>';
+    }
+    d.setDate(d.getDate() + 1);
+  }
+
+  table += '</tr></tbody></table>';
+  elem.innerHTML = table;
+}
+
+function getDay(date) {
+  let day = date.getDay();
+  if (day == 0) day = 7;
+  return day - 1;
+}
+
+// createCalendar('.calendar__item', 2023, 11);
