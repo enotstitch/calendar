7afb22914d0bd8b8f36d72d14e9ad281ef40214d
ZayRexan <mogilevkostya@yandex.ru>
2023-11-20 15:16:49 +0700

    Правки в стилях

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function createCalendar(elem, year, month) {
-  elem = document.querySelector(elem);
+	elem = document.querySelector(elem);
 
-  let mon = month - 1;
-  let d = new Date(year, mon);
+	let mon = month - 1;
+	let d = new Date(year, mon);
 
-  let table = `
+	let table = `
     <table>
     <tbody>
       <tr>
@@ -19,26 +19,27 @@ function createCalendar(elem, year, month) {
       <tr>
   `;
 
-  for (let i = 0; i < getDay(d); i++) {
-    table += '<td></td>';
-  }
+	for (let i = 0; i < getDay(d); i++) {
+		table += '<td></td>';
+	}
 
-  while (d.getMonth() == mon) {
-    table += '<td>' + d.getDate() + '</td>';
-    if (getDay(d) % 7 == 6) {
-      table += '</tr><tr>';
-    }
-    d.setDate(d.getDate() + 1);
-  }
+	while (d.getMonth() == mon) {
+		table += '<td>' + d.getDate() + '</td>';
+		if (getDay(d) % 7 == 6) {
+			table += '</tr><tr>';
+		}
+		d.setDate(d.getDate() + 1);
+	}
 
-  table += '</tr></tbody></table>';
-  elem.innerHTML = table;
+	table += '</tr></tbody></table>';
+	elem.innerHTML = table;
 }
 
 function getDay(date) {
-  let day = date.getDay();
-  if (day == 0) day = 7;
-  return day - 1;
+	let day = date.getDay();
+	if (day == 0) day = 7;
+	return day - 1;
 }
 
-// createCalendar('.calendar__item', 2023, 11);
+createCalendar('.calendar__item-first', 2023, 11);
+createCalendar('.calendar__item-second', 2023, 11);
