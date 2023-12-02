f7113bcb4740a9104946565686d7143a60f800b4
ZayRexan <mogilevkostya@yandex.ru>
2023-12-02 15:56:51 +0700

    Правки в селектах

--- js/select.js
+++ js/select.js
@@ -2,18 +2,18 @@ const now = new Date();
 const nowYear = now.getFullYear();
 
 const month = [
-  'Январь',
-  'Февраль',
-  'Март',
-  'Апрель',
-  'Май',
-  'Июнь',
-  'Июль',
-  'Август',
-  'Сентябрь',
-  'Октябрь',
-  'Ноябрь',
-  'Декабрь',
+	'Январь',
+	'Февраль',
+	'Март',
+	'Апрель',
+	'Май',
+	'Июнь',
+	'Июль',
+	'Август',
+	'Сентябрь',
+	'Октябрь',
+	'Ноябрь',
+	'Декабрь',
 ];
 
 const years = [];
@@ -21,120 +21,128 @@ const years = [];
 let minYear = nowYear - 20;
 
 for (let year = nowYear; year > minYear; year--) {
-  years.push(year);
+	years.push(year);
 }
 
 let select = function () {
-  let selectHeader = document.querySelectorAll('.select-current');
-  let selectItem = document.querySelectorAll('.select__item');
-  let selectBtnsNext = document.querySelectorAll('.select__button--next');
-  let selectBtnsPrev = document.querySelectorAll('.select__button--prev');
+	let selectHeader = document.querySelectorAll('.select-current');
+	let selectItem = document.querySelectorAll('.select__item');
+	let selectBtnsNext = document.querySelectorAll('.select__button--next');
+	let selectBtnsPrev = document.querySelectorAll('.select__button--prev');
 
-  let i = 0;
+	let i = 0;
 
-  window.addEventListener('click', (event) => {
-    try {
-      const select = document.querySelector('.select');
-      const isSelectActive = select.classList.contains('is-active');
-      const isSelectClick = event.target.closest('.select');
-      if (isSelectActive && !isSelectClick) {
-        select.classList.remove('is-active');
-      }
-    } catch {}
-  });
+	window.addEventListener('click', (event) => {
+		try {
+			const select = document.querySelector('.select');
+			const isSelectActive = select.classList.contains('is-active');
+			const isSelectClick = event.target.closest('.select');
+			if (isSelectActive && !isSelectClick) {
+				select.classList.remove('is-active');
+			}
+		} catch {}
+	});
 
-  selectHeader.forEach((item) => {
-    item.addEventListener('click', selectToggle);
-  });
+	selectHeader.forEach((item) => {
+		item.addEventListener('click', selectToggle);
+	});
 
-  selectItem.forEach((item) => {
-    item.addEventListener('click', selectChoose);
-  });
+	selectItem.forEach((item) => {
+		item.addEventListener('click', selectChoose);
+	});
 
-  function selectToggle() {
-    const select = this.parentElement.closest('.select');
-    select.classList.toggle('is-active');
-  }
+	function selectToggle() {
+		const select = this.parentElement.closest('.select');
+		select.classList.toggle('is-active');
+	}
 
-  function selectChoose() {
-    let text = this.innerText,
-      select = this.closest('.select'),
-      currentText = select.querySelector('.select-current__text');
-    currentText.innerText = text;
-    select.classList.remove('is-active');
+	function selectChoose() {
+		let text = this.innerText,
+			select = this.closest('.select'),
+			currentText = select.querySelector('.select-current__text');
+		currentText.innerText = text;
+		select.classList.remove('is-active');
 
-    const selectWrap = select.closest('.calendar__selects');
-    try {
-      const disabledSelect = selectWrap.querySelector('.disabled');
-      disabledSelect.classList.remove('disabled');
-    } catch {}
-  }
+		const selectWrap = select.closest('.calendar__selects');
+		try {
+			const disabledSelect = selectWrap.querySelector('.disabled');
+			disabledSelect.classList.remove('disabled');
+		} catch {}
+	}
 
-  selectBtnsNext.forEach((selectBtnNext) => {
-    selectBtnNext.addEventListener('click', (e) => {
-      const currentSelect = e.target.closest('.select');
-      const currentSelectItems =
-        currentSelect.querySelectorAll('.select__item');
-      const currentSelectText = currentSelect.querySelector(
-        '.select-current__text'
-      );
-      const selectWrap = e.target.closest('.calendar__selects');
-      const isYearSelect = currentSelect.closest('[data-year-select]');
-      const isMonthSelect = currentSelect.closest('[data-month-select]');
+	selectBtnsNext.forEach((selectBtnNext) => {
+		selectBtnNext.addEventListener('click', (e) => {
+			const currentSelect = e.target.closest('.select');
+			const currentSelectItems =
+				currentSelect.querySelectorAll('.select__item');
+			const currentSelectText = currentSelect.querySelector(
+				'.select-current__text'
+			);
+			const selectWrap = e.target.closest('.calendar__selects');
+			const isYearSelect = currentSelect.closest('[data-year-select]');
+			const isMonthSelect = currentSelect.closest('[data-month-select]');
 
-      if (isYearSelect) {
-        let indexOfItem = years.indexOf(+currentSelectText.textContent);
-        i = indexOfItem++;
-      }
-      if (isMonthSelect) {
-        let indexOfItem = month.indexOf(currentSelectText.textContent);
-        i = indexOfItem++;
-      }
+			if (isYearSelect) {
+				let indexOfItem = years.indexOf(+currentSelectText.textContent);
+				i = indexOfItem++;
+			}
+			if (isMonthSelect) {
+				let indexOfItem = month.indexOf(currentSelectText.textContent);
+				i = indexOfItem++;
+			}
 
-      try {
-        currentSelectText.textContent = currentSelectItems[--i].textContent;
-        const disabledSelect = selectWrap.querySelector('.disabled');
-        try {
-          disabledSelect.classList.remove('disabled');
-        } catch {}
-      } catch {
-        i = 0;
-      }
-    });
-  });
+			try {
+				if (isYearSelect) {
+					currentSelectText.textContent = currentSelectItems[--i].textContent;
+				} else if (isMonthSelect) {
+					currentSelectText.textContent = currentSelectItems[++i].textContent;
+				}
+				const disabledSelect = selectWrap.querySelector('.disabled');
+				try {
+					disabledSelect.classList.remove('disabled');
+				} catch {}
+			} catch {
+				i = 0;
+			}
+		});
+	});
 
-  selectBtnsPrev.forEach((selectBtnPrev) => {
-    selectBtnPrev.addEventListener('click', (e) => {
-      const currentSelect = e.target.closest('.select');
-      const currentSelectItems =
-        currentSelect.querySelectorAll('.select__item');
-      const currentSelectText = currentSelect.querySelector(
-        '.select-current__text'
-      );
-      const selectWrap = e.target.closest('.calendar__selects');
-      const isYearSelect = currentSelect.closest('[data-year-select]');
-      const isMonthSelect = currentSelect.closest('[data-month-select]');
+	selectBtnsPrev.forEach((selectBtnPrev) => {
+		selectBtnPrev.addEventListener('click', (e) => {
+			const currentSelect = e.target.closest('.select');
+			const currentSelectItems =
+				currentSelect.querySelectorAll('.select__item');
+			const currentSelectText = currentSelect.querySelector(
+				'.select-current__text'
+			);
+			const selectWrap = e.target.closest('.calendar__selects');
+			const isYearSelect = currentSelect.closest('[data-year-select]');
+			const isMonthSelect = currentSelect.closest('[data-month-select]');
 
-      if (isYearSelect) {
-        let indexOfItem = years.indexOf(+currentSelectText.textContent);
-        i = indexOfItem++;
-      }
-      if (isMonthSelect) {
-        let indexOfItem = month.indexOf(currentSelectText.textContent);
-        i = indexOfItem++;
-      }
+			if (isYearSelect) {
+				let indexOfItem = years.indexOf(+currentSelectText.textContent);
+				i = indexOfItem++;
+			}
+			if (isMonthSelect) {
+				let indexOfItem = month.indexOf(currentSelectText.textContent);
+				i = indexOfItem++;
+			}
 
-      try {
-        currentSelectText.textContent = currentSelectItems[++i].textContent;
-        const disabledSelect = selectWrap.querySelector('.disabled');
-        try {
-          disabledSelect.classList.remove('disabled');
-        } catch {}
-      } catch {
-        i = currentSelectItems.length - 1;
-      }
-    });
-  });
+			try {
+				if (isYearSelect) {
+					currentSelectText.textContent = currentSelectItems[++i].textContent;
+				} else if (isMonthSelect) {
+					currentSelectText.textContent = currentSelectItems[--i].textContent;
+				}
+				const disabledSelect = selectWrap.querySelector('.disabled');
+				try {
+					disabledSelect.classList.remove('disabled');
+				} catch {}
+			} catch {
+				i = currentSelectItems.length - 1;
+			}
+		});
+	});
 };
 
 export default select;
